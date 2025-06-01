8f791945d7478f0dad97a2f271359889c16ae2a6
Kerenza13 <carlichesjunior@gmail.com>
2025-06-02 01:20:40 +0200

    fix: Update environment variables, Dockerfile, SQL initialization, and Nginx configuration for improved setup

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,9 @@
+CREATE DATABASE IF NOT EXISTS RECUC_BD;
+USE RECUC_BD;
+
 CREATE TABLE secretosRECUC (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fraseRECUC VARCHAR(255) NOT NULL
 );
+
 INSERT INTO secretosRECUC (fraseRECUC) VALUES ('La base de datos responde, buenos días Carlos Morillas Delgado');
